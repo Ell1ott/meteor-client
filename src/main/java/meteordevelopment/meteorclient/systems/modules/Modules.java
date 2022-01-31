0bc2a846648c7d8be47d89e5e500585d9716097b
Ell1ott <Elliott.friedrich@outlook.dk>
2022-01-31 12:58:33 +0100

    added minehutautojoin

--- src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
@@ -419,7 +419,7 @@ public class Modules extends System<Modules> {
         add(new Reach());
         add(new Rotation());
         add(new SpeedMine());
-        add(new sendip());
+        add(new minehutautojoin());
     }
 
     private void initMovement() {
