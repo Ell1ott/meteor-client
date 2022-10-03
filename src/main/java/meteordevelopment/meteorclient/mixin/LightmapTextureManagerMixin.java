dadd2891e376c0a6910f1e9e62078625f7145eba
Elliott <dbtracker.dk@gmail.com>
2022-10-03 16:36:03 +0200

    fixed fullbrigth

--- src/main/java/meteordevelopment/meteorclient/mixin/LightmapTextureManagerMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/LightmapTextureManagerMixin.java
@@ -2,14 +2,11 @@
  * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
  * Copyright (c) Meteor Development.
  */
-
 package meteordevelopment.meteorclient.mixin;
-
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.render.Fullbright;
 import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import meteordevelopment.meteorclient.systems.modules.render.Xray;
-import meteordevelopment.meteorclient.utils.render.color.Color;
 import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,29 +14,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
-
 @Mixin(LightmapTextureManager.class)
 public class LightmapTextureManagerMixin {
     @ModifyArgs(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/texture/NativeImage;setColor(III)V"))
     private void update(Args args) {
         if (Modules.get().get(Fullbright.class).getGamma() || Modules.get().isActive(Xray.class)) {
-
-             args.set(2, Math.min(Integer.decode(args.get(2).toString()) + (Integer)(getIntFromColor(Modules.get().get(Fullbright.class).getGammaA(), Modules.get().get(Fullbright.class).getGammaA(), Modules.get().get(Fullbright.class).getGammaA())), 0xFFFFFFFF));
+            args.set(2, 0xFFFFFFFF);
         }
     }
-
     @Inject(method = "getDarknessFactor(F)F", at = @At("HEAD"), cancellable = true)
 	private void getDarknessFactor(float tickDelta, CallbackInfoReturnable<Float> info) {
 		if (Modules.get().get(NoRender.class).noDarkness()) info.setReturnValue(0.0f);
 	}
-
-    public int getIntFromColor(int Red, int Green, int Blue){
-        Red = (Red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
-        Green = (Green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
-        Blue = Blue & 0x000000FF; //Mask out anything not blue.
-
-        return 0xFF000000 | Red | Green | Blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
-    }
-
-
 }
